be31cec51291a7148762dd7b69cff9758563fff2
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-11 12:54:05 -0800

    Cleaned input file more by stripping mid-line parentheses and brackets

--- friendkov.py
+++ friendkov.py
@@ -1,5 +1,6 @@
-#from markov.py import *
+from markov import *
 import re
+import pprint
 
 
 def open_and_clean_file(filename):
@@ -15,9 +16,14 @@ def open_and_clean_file(filename):
                 speech_only.append(line)
     for line in speech_only:
         m = re.findall(r'\((.*?)\)', line)
+        n = re.findall(r'\[(.*?)\]', line)
+        print m
         if m != []:
             for i in range(len(m)):
                 line = line.replace("(" + m[i] + ")", "")
+        if n != []:
+            for j in range(len(n)):
+                line = line.replace("[" + n[j] + "]", "")
         cleaned_speech.append(line)
             #print line
 
@@ -34,12 +40,14 @@ def build_dictionary(clean_file):
             character_name = line.split(":")[0]
             # print character_name
             script_line = line.split(":")[1]
-            character_dic.setdefault(character_name, [])
-            character_dic[character_name].append(script_line)
+            character_dic.setdefault(character_name, "")
+            character_dic[character_name] += script_line
     return character_dic
 
 
 clean_file= open_and_clean_file("friends.txt")
-build_dictionary(clean_file)
-
+character_dic = build_dictionary(clean_file)
+for character in character_dic.keys():
+    character_dic[character] = make_chains(character_dic[character], 2)
+pprint.pprint(character_dic)
 # \((.*?)\)
