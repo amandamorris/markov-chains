6c7b6b900c719aafa8261140cab1127d53936734
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-11 12:21:26 -0800

    BUild dictionary that every character has all their lines.

--- friendkov.py
+++ friendkov.py
@@ -2,10 +2,11 @@
 import re
 
 
-def open_file(filename):
+def open_and_clean_file(filename):
     script = open(filename)
 
     speech_only = []
+    cleaned_speech = []
     for line in script:
         #print line
         line = line.strip()
@@ -17,10 +18,28 @@ def open_file(filename):
         if m != []:
             for i in range(len(m)):
                 line = line.replace("(" + m[i] + ")", "")
-            print line
+        cleaned_speech.append(line)
+            #print line
 
-    #print speech_only
+    return cleaned_speech
 
-open_file("friends.txt")
+
+def build_dictionary(clean_file):
+
+    character_dic = {}
+    for line in clean_file:
+        #print line
+        #print line.find(":")
+        if line.find(":") != -1:
+            character_name = line.split(":")[0]
+            # print character_name
+            script_line = line.split(":")[1]
+            character_dic.setdefault(character_name, [])
+            character_dic[character_name].append(script_line)
+    return character_dic
+
+
+clean_file= open_and_clean_file("friends.txt")
+build_dictionary(clean_file)
 
 # \((.*?)\)
